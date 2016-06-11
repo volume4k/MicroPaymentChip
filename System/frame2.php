94c7286610914bc105201a90aa3848bdadcccdf7
Jonathan Kowa <www.jonathan-kowa@gmx.de>
2016-06-11 14:42:43 +0200

    working Database penetration

--- System/frame2.php
+++ System/frame2.php
@@ -6,13 +6,15 @@ $password="Banane66";
 $database="MPC";
 
 //Create new connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+
 ?>
 <br/>
 <?php
@@ -20,9 +22,16 @@ echo "Connected successfully";
 
 
 $reference=rand(-1000000000000000,-99999999999999999999999999999999);
-echo $reference;
 $hashedref=md5($reference);
 echo $hashedref;
-
+?>
+<br/>
+<?php
+$sql = "INSERT INTO reference_number (id, referencenumber) VALUES ('', '$hashedref')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 ?>
