dd3ca30b54adc813f3e5a03e104ed04a0b2df8d4
Jonathan Kowa <www.jonathan-kowa@gmx.de>
2016-06-11 12:40:18 +0200

    System
    Added 1st try of some systemati code

---
+++ System/frame1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+  <form name="form" action="" method="get">
+  <input type="text" name="subject" id="subject" value="Car Loan">
+</form>
+
+<?php echo $_GET['subject'];
+$system=$_GET["subject"];
+echo $system;
+
+?>
