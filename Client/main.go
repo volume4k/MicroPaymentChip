343a3bc09d215d4a909746c59c338c909dc30a9b
Jonathan Kowa <jonathan@kowa.io>
2020-08-09 17:30:42 +0200

    Add Card Validation

--- Client/main.go
+++ Client/main.go
@@ -26,11 +26,34 @@ func handle(){
 func openToReadCard() {
 	c := make(chan []int, 2)
 	go cardInformationReceived(c)
-	fmt.Println(<- c)
+	n := <- c
+	fmt.Println(n)
+	if creditCardIsValid(n) {
+		fmt.Println("CARD VALID... processing")
+		time.Sleep(1 * time.Second)
+		fmt.Println("Transaction successfully processed.")
+	}
 }
 
 func cardInformationReceived(c chan []int) {
 	time.Sleep(10 * time.Second)
 	n := []int{5478 , 5005100120018501}
 	c <- n
+}
+
+func creditCardIsValid(i []int) bool {
+	if lengthOfInt(i[0]) != 4 && lengthOfInt(i[1]) != 16 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func lengthOfInt(i int) int {
+	n := 0
+	for i != 0 {
+		n++
+		i--
+	}
+	return n
 }
